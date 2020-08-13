ef0e0749eecafe4dd28d2245eabf13c1f9651dff
Rex109 <andrea.nalin@hotmail.it>
2020-08-13 23:39:30 +0200

    Added IP cameras support

--- COM3D2.VyperCam.Plugin/COM3D2.VyperCam.Plugin.cs
+++ COM3D2.VyperCam.Plugin/COM3D2.VyperCam.Plugin.cs
@@ -6,20 +6,27 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using ExIni;
 using System.Linq;
+using System.Net;
+using System.IO;
+using System.Collections;
 
 namespace COM3D2.VyperCam.Plugin
 {
-    [PluginFilter("COM3D2x64"), PluginFilter("COM3D2VRx64"), PluginFilter("COM3D2OHx64"), PluginFilter("COM3D2OHVRx64"), PluginName("VyperCam"), PluginVersion("1.1.0.0")]
+    [PluginFilter("COM3D2x64"), PluginFilter("COM3D2VRx64"), PluginFilter("COM3D2OHx64"), PluginFilter("COM3D2OHVRx64"), PluginName("VyperCam"), PluginVersion("1.2.0.0")]
     public class VyperCam : PluginBase
     {
         private WebCamDevice[] devices = WebCamTexture.devices;
         private static WebCamTexture webcamTex;
+        private static Texture2D ipwebcamTex;
 
         private bool objectcreated = false;
 
         private GameObject panel;
 
         private KeyCode togglekey = KeyCode.F6;
+        private int WebcamMode = 0;
+        private String webcamURL = "";
+
         private int webcamid = 0;
         private float camsizex = 500f;
         private float camsizey = 500f;
@@ -27,6 +34,9 @@ namespace COM3D2.VyperCam.Plugin
         private float camoffsety = 0f;
 
         private bool vr;
+
+        private Stream stream;
+        private RawImage ImageRect;
 
         public void Start()
         {
@@ -42,13 +52,16 @@ namespace COM3D2.VyperCam.Plugin
             {
                 LoadConfig();
 
-                if (devices.Length > 0)
+                if (WebcamMode == 0)
                 {
-                    webcamTex = new WebCamTexture(WebCamTexture.devices[webcamid].name);
-                    Console.WriteLine("[VyperCam] Webcam loaded: " + webcamTex.deviceName);
+                    if (devices.Length > 0)
+                    {
+                        webcamTex = new WebCamTexture(WebCamTexture.devices[webcamid].name);
+                        Console.WriteLine("[VyperCam] Webcam loaded: " + webcamTex.deviceName);
+                    }
+                    else
+                        Console.WriteLine("[VyperCam] Can't find any camera");
                 }
-                else
-                    Console.WriteLine("[VyperCam] Can't find any camera");
             }
         }
 
@@ -57,6 +70,8 @@ namespace COM3D2.VyperCam.Plugin
             try
             {
                 IniKey key = Preferences["Config"]["ToggleKey"];
+                IniKey webmode = Preferences["Config"]["WebcamMode"];
+                IniKey weburl = Preferences["Config"]["WebcamURL"];
                 IniKey id = Preferences["Config"]["WebcamID"];
                 IniKey sizex = Preferences["Config"]["WebcamSizeX"];
                 IniKey sizey = Preferences["Config"]["WebcamSizeY"];
@@ -69,6 +84,8 @@ namespace COM3D2.VyperCam.Plugin
                 camoffsetx = float.Parse(offsetx.Value);
                 camoffsety = float.Parse(offsety.Value);
 
+                WebcamMode = int.Parse(webmode.Value);
+                webcamURL = weburl.Value;
 
                 togglekey = (KeyCode)Enum.Parse(typeof(KeyCode), key.Value, true);
                 webcamid = int.Parse(id.Value);
@@ -108,16 +125,41 @@ namespace COM3D2.VyperCam.Plugin
                     rect.pivot = new Vector2(1, 0);
                     rect.localPosition = new Vector2(960 + camoffsetx, -540 + camoffsety);
 
-                    var image = panel.AddComponent<RawImage>();
-                    image.transform.SetParent(panel.transform, false);
-                    image.material.mainTexture = webcamTex;
-                    image.enabled = false;
+                    ImageRect = panel.AddComponent<RawImage>();
+                    ImageRect.transform.SetParent(panel.transform, false);
+                    ImageRect.enabled = false;
                     objectcreated = true;
                 }
 
-                webcamTex.Play();
+                if (WebcamMode == 0)
+                {
+                    ImageRect.material.mainTexture = webcamTex;
+                    webcamTex.Play();
+                }
+                else if (WebcamMode == 1)
+                {
+                    StopCoroutine(Fetch()); //Just to prevent overlapping coroutines
+                    ipwebcamTex = new Texture2D(2, 2);
+                    ImageRect.material.mainTexture = ipwebcamTex;
+                    StartCoroutine(Fetch());
+                }
 
                 Console.WriteLine("[VyperCam] Done!");
+            }
+        }
+
+        public IEnumerator Fetch()
+        {
+            while (true)
+            {
+                WWWForm form = new WWWForm();
+                WWW www = new WWW(webcamURL);
+                yield return www;
+
+                if (!string.IsNullOrEmpty(www.error))
+                    throw new UnityException(www.error);
+
+                www.LoadImageIntoTexture(ipwebcamTex);
             }
         }
 
